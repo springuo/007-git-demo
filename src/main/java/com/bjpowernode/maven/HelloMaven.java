0aaeb2aa8661860a6140ab2743c99b11fffe2740
bj <bj@126.com>
2018-09-03 22:07:06 +0800

    007-IDEA整合Git测试

---
+++ src/main/java/com/bjpowernode/maven/HelloMaven.java
@@ -0,0 +1,17 @@
+package com.bjpowernode.maven;
+
+/**
+ * ClassName:HelloMaven
+ * Package:com.bjpowernode.maven
+ * Description:
+ *
+ * @date 2018/9/3 21:54
+ * @author guoxin
+ */
+public class HelloMaven {
+
+    public static void main(String[] args) {
+        System.out.println("----Hello Git Maven Java Project!!---");
+    }
+
+}
