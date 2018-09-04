3cb0ceaf3699971b3c2b44acc39f671f77355e2c
springuo <42195056+springuo@users.noreply.github.com>
2018-09-04 08:54:03 +0800

    Update HelloMaven.java

--- src/main/java/com/bjpowernode/maven/HelloMaven.java
+++ src/main/java/com/bjpowernode/maven/HelloMaven.java
@@ -12,7 +12,6 @@ public class HelloMaven {
 
     public static void main(String[] args) {
         System.out.println("----Hello Git Maven Java Project!!---");
-        System.out.println("----Hello Git Maven Java Project!!---");
     }
 
 }
